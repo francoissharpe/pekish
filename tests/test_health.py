2a81d41022e7a35d50141dd75ed9efd2dcac7ed6
Francois Sharpe <francois.sharpe@live.com>
2021-11-30 20:43:33 +0100

    remove a test

--- tests/test_health.py
+++ tests/test_health.py
@@ -5,10 +5,9 @@ def test_health_ping(client: TestClient):
     response = client.get("/health/ping")
     assert response.status_code == 200
     assert response.json() == {"message": "ok"}
-    
+
 
 # def test_health_report(client: TestClient):
 #     response = client.get("/health/report")
 #     assert response.status_code == 200
 #     assert response.json() == {"message": "ok"}
-
