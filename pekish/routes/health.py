dd7d8ddedb1007af49413da288ff2c743dd3e8d8
Francois Sharpe <francois.sharpe@live.com>
2021-11-30 20:20:39 +0100

    added report endpoint

--- pekish/routes/health.py
+++ pekish/routes/health.py
@@ -9,3 +9,8 @@ router = APIRouter()
 @router.get("/ping", response_model=schemas.Status)
 def ping():
     return schemas.Status(message="ok")
+
+
+@router.get("/report", response_model=schemas.Status)
+def report():
+    return schemas.Status(message="ok")
